0eac66f0e1bb7e0bf2ce805cd4dbc56d2ca8b3cf
adammcpherson <aomcpherson@gmail.com>
2018-10-24 17:10:13 -0500

    added login feature

---
+++ web/authenticate.php
@@ -0,0 +1,30 @@
+<?php
+/**********************************************************
+* File: authenticate.php
+* Author: Adam McPherson
+* 
+* Description: This is the login page and will hopefully be able 
+* to authenticate the user and allow for them to create their own 
+* shelves to put books on as well as adding books to the database.
+***********************************************************/
+
+require("dbConnect.php");
+$db = get_db();
+
+/***********************************************************
+* This is the query that I would like to run
+* SELECT id, username, password FROM users WHERE username = 
+*/
+$statement = $db->prepare("SELECT id, username, password FROM public.users WHERE username = " . $_POST['lg_username']) . "AND password = " . $_POST['lg_password'];
+$statement->execute();
+
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+    if ($row['username'] == $_POST['lg_username'] && 
+        $row['password'] == $_POST['lg_password']))
+        echo "LOGGED IN";
+    else
+        echo "INVALID PASSWORD";
+}
+
+?>
--- web/login.php
+++ web/login.php
@@ -45,7 +45,7 @@
 	<div class="logo">login</div>
 	<!-- Main Form -->
 	<div class="login-form-1">
-		<form id="login-form" class="text-left">
+		<form id="login-form" class="text-left" action="authenticate.php">
 			<div class="login-form-main-message"></div>
 			<div class="main-login-form">
 				<div class="login-group">
