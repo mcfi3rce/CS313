f3a6103a9b35310e20f9fc4173360d258a5f9798
adammcpherson <aomcpherson@gmail.com>
2018-10-24 21:03:08 -0500

    trying things Brother Burton's way

--- web/authenticate.php
+++ web/authenticate.php
@@ -15,10 +15,23 @@ $db = get_db();
 * This is the query that I would like to run
 * SELECT id, username, password FROM user WHERE username = 
 */
-if (isset($_POST['lg_username'])){
-    echo "SELECT id, username, password FROM user WHERE username = " . $_POST['lg_username'] . " AND password = " . $_POST['lg_password'];
+
+// Get the Data from the POST
+$username = $_POST['lg_username'];
+$password = $_POST['lg_password'];
+
+echo "username=$username\n";
+echo "password=$password\n";
+
     
-    $statement = $db->prepare("SELECT id, username, password FROM user WHERE username = " . $_POST['lg_username'] . " AND password = " . $_POST['lg_password']);
+if (isset($_POST['lg_username'])){
+    
+    $query = 'SELECT id, username, password FROM user WHERE username = :username AND password = :password';
+    $statement = $db->prepare($query);
+    
+    $statement->bindValue(':username', $username;
+	$statement->bindValue(':password', $password);
+	
     $statement->execute();
     echo "EXECUTE";
     while ($row = $statement->fetch(PDO::FETCH_ASSOC))
