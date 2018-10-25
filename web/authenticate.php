a1d35966d1abb3782d8f84160589c8cf4f01889e
adammcpherson <aomcpherson@gmail.com>
2018-10-24 20:32:44 -0500

    still debuggin my POST method that should be a GET

--- web/authenticate.php
+++ web/authenticate.php
@@ -17,7 +17,7 @@ $db = get_db();
 */
 if (isset($_POST['lg_username'])){
     
-    $statement = $db->prepare("SELECT id, username, password FROM public.users WHERE username = " . $_POST['lg_username']) . "AND password = " . $_POST['lg_password'];
+    $statement = $db->prepare("SELECT id, username, password FROM public.users WHERE username = " . $_POST['lg_username']) . " AND password = " . $_POST['lg_password'];
     $statement->execute();
     while ($row = $statement->fetch(PDO::FETCH_ASSOC))
     {
