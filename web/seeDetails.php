64b45967dbcfb60a27f0e0ef4d66e45e9946d3fe
adammcpherson <aomcpherson@gmail.com>
2018-11-03 15:05:29 -0500

    so far so good

---
+++ web/seeDetails.php
@@ -0,0 +1,14 @@
+<?php
+/**********************************************************
+* File: authenticate.php
+* Author: Adam McPherson
+* 
+* Description: This is the login page and will hopefully be able 
+* to authenticate the user and allow for them to create their own 
+* shelves to put books on as well as adding books to the database.
+***********************************************************/
+
+require("dbConnect.php");
+$db = get_db();
+
+echo $_POST['book_id'];
