eb764024dd88b672984c6ddb55079137f1e9916b
adammcpherson <aomcpherson@gmail.com>
2018-11-06 11:16:28 -0600

    forgot to link the style sheet for the thumb

--- web/seeDetails.php
+++ web/seeDetails.php
@@ -112,6 +112,7 @@ catch (Exception $ex)
   <title><?php echo $title;?></title>
   <meta charset="utf-8">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" type="text/css" href="seeDetails.css">
   <link href='https://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
