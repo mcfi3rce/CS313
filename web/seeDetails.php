b835ac9ab02f58835ebf13a6452c17d2531c56d3
adammcpherson <aomcpherson@gmail.com>
2018-11-06 18:10:45 -0600

    redirect from every page to login if not authenticated

--- web/seeDetails.php
+++ web/seeDetails.php
@@ -9,6 +9,11 @@
 ***********************************************************/
 
 session_start();
+
+if (!isset($_SESSION['logged_in'])){
+    header("Location: login.php");
+}
+
 require("dbConnect.php");
 $db = get_db();
 
